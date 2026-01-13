15b2425b9778c3ce7c8a8ac5ff4bb63e45c03979
Deepa <deepakamboj@gmail.com>
2026-01-13 21:16:07 +0530

    modified bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
         int nums[] ={6,4,8,9,2,5};
         int size = nums.length;
         int temp=0;
+
+        System.out.println("Before sorting : ");
+        for(int num:nums){
+            System.out.print(num +" ");
+        }
 
         for(int i=0;i<size;i++){
             for(int j=0;j<size-i-1;j++){
@@ -13,13 +20,15 @@ public class BubbleSort {
                 nums[j+1]=temp;
                 }
             }
-            System.out.println();
+            System.out.println((i+1)+"th iteration");
             for(int num:nums){
                 System.out.print(num+" ");
             }
         }
-
-
-
+        System.out.println();
+        System.out.println("After sorting : ");
+        for(int num:nums){
+            System.out.print(num +" ");
+        }
     }
 }
